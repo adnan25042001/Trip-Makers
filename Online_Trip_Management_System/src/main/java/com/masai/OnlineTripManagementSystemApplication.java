9c576175d0e2c1a4d083ccf76852cf6734d49ba0
Rakesh Gudur <rakeshgudur7420@gmail.com>
2022-12-13 22:03:52 +0530

    added

---
+++ Online_Trip_Management_System/src/main/java/com/masai/OnlineTripManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTripManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTripManagementSystemApplication.class, args);
+	}
+
+}
