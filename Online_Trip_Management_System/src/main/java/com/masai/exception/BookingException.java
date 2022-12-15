574b2609a3f8d50d0c7af0eda94a460c522de9d7
Rakesh Gudur <rakeshgudur7420@gmail.com>
2022-12-15 08:46:30 +0530

    day2

---
+++ Online_Trip_Management_System/src/main/java/com/masai/exception/BookingException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class BookingException extends Exception {
+
+	
+	public BookingException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BookingException(String msg) {
+		// TODO Auto-generated constructor stub
+		
+		super(msg);
+	}
+}
---
+++ Online_Trip_Management_System/src/main/java/com/masai/exception/PackageException.java
@@ -0,0 +1,14 @@
+package com.masai.exception;
+
+public class PackageException extends Exception {
+
+	
+	public PackageException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PackageException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
