a67ea5a7e774b4f743848a887761efc4d68284cf
Adnan <adnan.hussain.136660@gmail.com>
2022-12-15 15:25:53 +0530

    a

--- Online_Trip_Management_System/src/main/java/com/masai/model/Admin.java
+++ Online_Trip_Management_System/src/main/java/com/masai/model/Admin.java
@@ -40,7 +40,5 @@ public class Admin {
 
 	@Pattern(regexp = "[A-Za-z0-9@]{6,15}", message = "Password must be 6 to 15 characters and must have at least 1 alphabate and 1 number")
 	private String password;
-	
-	private 
 
 }
